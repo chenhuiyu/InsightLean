fa627c5af93b388319540ca4baac021caf87a1a9
Huiyu Chen <huiyu.chen@shopee.com>
2023-04-24 21:14:50 +0800

    format code

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
+import os
+from pathlib import Path
+
+import pinecone
 import streamlit as st
-from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma, Pinecone
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import OnlinePDFLoader, UnstructuredPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-import pinecone
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma, Pinecone
 
-import os
+DEFAULT_PDF_FILE = Path(os.path.dirname(__file__)) / \
+    "1-s2.0-S0920410521012511-main.pdf"
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
 PINECONE_ENVIRONMENT = os.environ['PINECONE_ENVIRONMENT']
 
-DEFAULT_PDF_URL = 'https://smallpdf.com/cn/file#s=6e5f1546-1092-439c-bf99-8aec701c73e6'
 
-st.title("Semantic Search and Question Answering")
-uploaded_file = st.file_uploader("Upload a PDF file or use the default file", type=['pdf'])
+st.title("InsightLean")
+uploaded_file = st.file_uploader(
+    "Upload a PDF file or use the default file", type=['pdf'])
 
-import os
-from pathlib import Path
-
-DEFAULT_PDF_FILE = Path(os.path.dirname(__file__)) / "1-s2.0-S0920410521012511-main.pdf"
 
 if uploaded_file:
     loader = UnstructuredPDFLoader(uploaded_file)
@@ -30,15 +30,18 @@ else:
 
 with st.spinner("Loading and processing the document..."):
     data = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(data)
 
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
     index_name = "insightlearn"
-    docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+    docsearch = Pinecone.from_texts(
+        [t.page_content for t in texts], embeddings, index_name=index_name)
 
-query = st.text_input("Enter your query:", value="Who is the author of this paper?")
+query = st.text_input("Enter your query:",
+                      value="Who is the author of this paper?")
 # model_option = st.radio("Choose a model:", ("GPT-4", "GPT3.5"))
 
 if query:
